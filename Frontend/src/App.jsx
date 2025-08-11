cf6eff45cba42d808cd75b10d5bb2331a68b1984
Kathleen <115058370+kaz123456789@users.noreply.github.com>
2025-08-11 16:58:29 -0700

    added router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
+      {/* / → render <Home />
+      /cart → render <Cart />
+      /order → render <PlaceOrder />*/}
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/order' element={<PlaceOrder/>} />
+      </Routes>
     </div>
   )
 }
