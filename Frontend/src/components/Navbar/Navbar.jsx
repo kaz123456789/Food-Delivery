93bc34e283aa252b9d09326f17bd5fc18e61c9ff
Kathleen <115058370+kaz123456789@users.noreply.github.com>
2025-08-11 16:37:13 -0700

    navbar

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 
 const Navbar = () => {
+    // useState: set states to navigate the corresponding changes
+    const [menu,setMenu] = useState("home"); 
   return (
     <div className='navbar'>
       <img src={assets.logo} alt="" className="logo" />
       <ul className='navbar-menu'>
-        <li>home</li>
-        <li>menu</li>
-        <li>mobile-app</li>
-        <li>contact</li>
+        <li onClick={()=>setMenu("home")} className={menu=="home"?"active":""}>home</li>
+        <li onClick={()=>setMenu("menu")} className={menu=="menu"?"active":""}>menu</li>
+        <li onClick={()=>setMenu("mobile-app")} className={menu=="mobile-app"?"active":""}>mobile-app</li>
+        <li onClick={()=>setMenu("contact-us")} className={menu=="contact-us"?"active":""}>contact</li>
       </ul>
-      <div className="naverbar-right">
+      <div className="navbar-right">
         <img src={assets.search_icon} alt="search icon"/>
         <div className="navbar-search-icon">
           <img src={assets.basket_icon} alt="bascket icon" />
