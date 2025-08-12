6067ddfc1a19fefb23fb7263dadc485d829c7e81
Kathleen <115058370+kaz123456789@users.noreply.github.com>
2025-08-11 17:08:50 -0700

    fixed route problem

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -4,7 +4,12 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
-      
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and satisfy your crazings and elevate your dining experience, one delicious meal at a time.</p>
+        <button>View Menu</button>
+        <img src="" alt="" className="header" />
+      </div>
     </div>
   )
 }
