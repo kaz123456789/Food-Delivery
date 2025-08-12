d42e8c7813e2b116dd5e873194cc25a318a7ee70
Kathleen <115058370+kaz123456789@users.noreply.github.com>
2025-08-11 17:31:16 -0700

    added header

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -6,9 +6,9 @@ const Header = () => {
     <div className='header'>
       <div className="header-contents">
         <h2>Order your favourite food here</h2>
-        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and satisfy your crazings and elevate your dining experience, one delicious meal at a time.</p>
+        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary experise. Our mission is to satisfy your crazings and elevate your dining experience, one delicious meal at a time.</p>
         <button>View Menu</button>
-        <img src="" alt="" className="header" />
+
       </div>
     </div>
   )
