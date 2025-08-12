7178abc36445cb29b7c498be40f14f254a2eb22c
Kathleen <115058370+kaz123456789@users.noreply.github.com>
2025-08-11 21:00:45 -0700

    home page items

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -60,6 +60,7 @@ import bag_icon from './bag_icon.png'
 import logout_icon from './logout_icon.png'
 import parcel_icon from './parcel_icon.png'
 
+// "export" here helps other files to inport the arrays and use them directily.
 export const assets = {
     logo,
     basket_icon,
